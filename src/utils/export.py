fb6ef0da43120269cd6bb201af2f612f6858daa5
Angelo Maria Iannone <angelo.iann@gmail.com>
2026-05-26 18:51:08 +0200

    Added utils and an onboarding notebook

---
+++ src/utils/export.py
@@ -0,0 +1,38 @@
+from enum import Enum
+from pathlib import Path
+import matplotlib.pyplot as plt
+import pandas as pd
+
+'''
+Utility class that helps save results for the paper in the correct folder.
+Works for figures, tables etc. 
+'''
+class Directory(Enum):
+        FIGURE_DIR = Path("paper/figures")
+        TABLE_DIR = Path("paper/tables")
+        
+class PathBuilder:
+
+    @staticmethod
+    def build(directory: Directory, filename) -> Path: 
+        return Path(directory.value) / filename
+
+def save_figure(filename: str):
+    """
+    Save current matplotlib figure to figure directory
+    """
+    path = PathBuilder.build(Directory.FIGURE_DIR, filename=f"{filename}.pdf")
+
+    plt.savefig(path) 
+
+    print(f"Figure: {filename} saved to: {path}")
+
+def save_table(df: pd.DataFrame, filename: str, **kwargs):
+    """
+    Save pandas dataframe as LaTeX table to paper/tables.
+    """
+    path = PathBuilder.build(directory=Directory.TABLE_DIR, filename=f"{filename}.tex")
+
+    df.to_latex(path)
+
+    print(f'Table: {filename} saved to: {path}')
